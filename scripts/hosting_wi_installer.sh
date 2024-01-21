5943859eee7d0cf9d457f59cafc8b97c3147c4bc
NequZ <nici.friedrich@hotmail.de>
2024-01-21 13:01:52 -0500

    Added installer File for later Preparation

---
+++ scripts/hosting_wi_installer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# ________   _______   ________  ___  ___  ________
+# |\   ___  \|\  ___ \ |\   __  \|\  \|\  \|\_____  \
+# \ \  \\ \  \ \   __/|\ \  \|\  \ \  \\\  \\|___/  /|
+# \ \  \\ \  \ \  \_|/_\ \  \\\  \ \  \\\  \   /  / /
+# \ \  \\ \  \ \  \_|\ \ \  \\\  \ \  \\\  \ /  /_/__
+# \ \__\\ \__\ \_______\ \_____  \ \_______\\________\
+# \|__| \|__|\|_______|\|___| \__\|_______|\|_______|
+# \|__|
+
+# Project : NequZ - WI
+# Created : 18.01.2024
+# Author  : NequZ
+#
+# Copyright (c) 2024 NequZ. All rights reserved.
+
+# This script is responsible for installing the necessary packages for the Interface.
+
+
